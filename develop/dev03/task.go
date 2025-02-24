666ec1e306a5643b0960b018b4686205b9709412
Dazhy Khovalyg <hovalyg_d@mail.ru>
2025-02-24 22:23:58 +0300

    dev03

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -38,9 +38,7 @@ import (
 	"unicode"
 )
 
-// Вспомогательная структура
-// для хранения всех опций сортировки, реализованы функции, чтобы можно было
-// использовать паке sort на нашей структуре
+// customSort хранит параметры сортировки и реализует интерфейс sort.Interface
 type customSort struct {
 	Lines             []string
 	Column            int
@@ -48,8 +46,8 @@ type customSort struct {
 	Reverse           bool
 	Unique            bool
 	Month             bool
-	Ignore            bool
-	Sorted            bool
+	IgnoreSpaces      bool
+	CheckSorted       bool
 	NumericWithSuffix bool
 }
 
@@ -62,46 +60,44 @@ func (cs *customSort) Swap(i, j int) {
 }
 
 func (cs *customSort) Less(i, j int) bool {
-	fieldI := strings.Fields(cs.Lines[i])[cs.Column-1]
-	fieldJ := strings.Fields(cs.Lines[j])[cs.Column-1]
+	fieldsI := strings.Fields(cs.Lines[i])
+	fieldsJ := strings.Fields(cs.Lines[j])
+
+	if cs.Column > len(fieldsI) || cs.Column > len(fieldsJ) {
+		return false
+	}
+
+	fieldI := fieldsI[cs.Column-1]
+	fieldJ := fieldsJ[cs.Column-1]
 
 	if cs.Numeric {
 		numI, errI := strconv.ParseFloat(fieldI, 64)
 		numJ, errJ := strconv.ParseFloat(fieldJ, 64)
-		if errI != nil {
-			log.Fatal(errI)
+		if errI != nil || errJ != nil {
+			log.Fatal("Ошибка преобразования в число")
 		}
-		if errJ != nil {
-			log.Fatal(errJ)
-		}
-
-		return numI < numJ != cs.Reverse
+		return (numI < numJ) != cs.Reverse
 	}
 
 	if cs.Month {
 		timeI, errI := time.Parse("Jan", fieldI)
 		timeJ, errJ := time.Parse("Jan", fieldJ)
-		if errI != nil {
-			log.Fatal(errI)
+		if errI != nil || errJ != nil {
+			log.Fatal("Ошибка обработки месяца")
 		}
-
-		if errJ != nil {
-			log.Fatal(errJ)
-		}
-
-		return timeI.Before(timeJ) != cs.Reverse
+		return (timeI.Before(timeJ)) != cs.Reverse
 	}
 
 	if cs.NumericWithSuffix {
 		numI := parseWithSuffix(fieldI)
 		numJ := parseWithSuffix(fieldJ)
-
-		return numI < numJ != cs.Reverse
+		return (numI < numJ) != cs.Reverse
 	}
 
-	return fieldI < fieldJ != cs.Reverse
+	return (fieldI < fieldJ) != cs.Reverse
 }
 
+// parseWithSuffix конвертирует числа с суффиксами (K, M, G и т. д.)
 func parseWithSuffix(s string) float64 {
 	s = strings.TrimSpace(s)
 	var i int
@@ -114,7 +110,7 @@ func parseWithSuffix(s string) float64 {
 	suffixPart := s[i:]
 	num, err := strconv.ParseFloat(numPart, 64)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Ошибка обработки числа с суффиксом")
 	}
 
 	switch strings.ToUpper(suffixPart) {
@@ -135,11 +131,12 @@ func parseWithSuffix(s string) float64 {
 	return num
 }
 
+// removeDuplicates удаляет дубликаты из списка строк
 func removeDuplicates(data *customSort) {
 	lines := make(map[string]struct{})
 	var uniqueLines []string
 	for _, line := range data.Lines {
-		if _, ok := lines[line]; !ok {
+		if _, exists := lines[line]; !exists {
 			lines[line] = struct{}{}
 			uniqueLines = append(uniqueLines, line)
 		}
@@ -147,64 +144,87 @@ func removeDuplicates(data *customSort) {
 	data.Lines = uniqueLines
 }
 
-func main() {
-	data := customSort{}
-	data.Column = *flag.Int("k", 1, "coulmn for sort")
-	data.Numeric = *flag.Bool("n", false, "sort by numeric value")
-	data.Reverse = *flag.Bool("r", false, "sort in reverse order")
-	data.Unique = *flag.Bool("u", false, "output only unique lines")
-	data.Month = *flag.Bool("M", false, "sort by month name")
-	data.Ignore = *flag.Bool("b", false, "ignore trailing spaces")
-	data.Sorted = *flag.Bool("c", false, "check if sorted")
-	data.NumericWithSuffix = *flag.Bool("h", false, "sort by numeric value with suffix")
+// parseFlags считывает аргументы командной строки и возвращает структуру с настройками
+func parseFlags() *customSort {
+	data := &customSort{}
+	flag.IntVar(&data.Column, "k", 1, "Столбец для сортировки (по умолчанию 1)")
+	flag.BoolVar(&data.Numeric, "n", false, "Сортировка по числовому значению")
+	flag.BoolVar(&data.Reverse, "r", false, "Обратная сортировка")
+	flag.BoolVar(&data.Unique, "u", false, "Вывод только уникальных строк")
+	flag.BoolVar(&data.Month, "M", false, "Сортировка по названию месяца")
+	flag.BoolVar(&data.IgnoreSpaces, "b", false, "Игнорировать завершающие пробелы")
+	flag.BoolVar(&data.CheckSorted, "c", false, "Проверить, отсортирован ли файл")
+	flag.BoolVar(&data.NumericWithSuffix, "h", false, "Числовая сортировка с учетом суффиксов (K, M, G и т. д.)")
 
 	flag.Parse()
+	return data
+}
 
-	/*args := flag.Args()
-	if len(args) != 1 {
-		log.Fatal("only the file name is allowed")
-	}
-
-	filename := args[0]
-	*/
-	filename := "test"
+// readLines читает строки из файла и возвращает их
+func readLines(filename string, ignoreSpaces bool) ([]string, error) {
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("ошибка открытия файла: %w", err)
 	}
-
 	defer file.Close()
 
+	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if data.Ignore {
+		if ignoreSpaces {
 			line = strings.TrimRight(line, " ")
 		}
-		data.Lines = append(data.Lines, line)
+		lines = append(lines, line)
 	}
 
-	if err = scanner.Err(); err != nil {
-		log.Fatal(err)
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("ошибка чтения файла: %w", err)
 	}
 
-	if data.Sorted {
-		sorted := sort.IsSorted(&data)
-		if sorted {
-			fmt.Println("Sorted")
-		} else {
-			fmt.Println("Not sorted")
-		}
-		return
-	}
+	return lines, nil
+}
 
-	sort.Sort(&data)
+// checkIfSorted проверяет, отсортирован ли файл
+func checkIfSorted(data *customSort) {
+	if sort.IsSorted(data) {
+		fmt.Println("Файл отсортирован")
+	} else {
+		fmt.Println("Файл не отсортирован")
+	}
+}
+
+// sortAndPrint выполняет сортировку и выводит результат
+func sortAndPrint(data *customSort) {
+	sort.Sort(data)
 
 	if data.Unique {
-		removeDuplicates(&data)
+		removeDuplicates(data)
 	}
 
 	for _, line := range data.Lines {
 		fmt.Println(line)
 	}
+}
+
+func mySort(filename string) {
+	data := parseFlags()
+
+	lines, err := readLines(filename, data.IgnoreSpaces)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data.Lines = lines
+
+	if data.CheckSorted {
+		checkIfSorted(data)
+		return
+	}
+
+	sortAndPrint(data)
+}
+
+func main() {
+	mySort("test1") // числа
+	//mySort("test2") // месяцы
 }
