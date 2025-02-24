1b918e4dab0ff18623aa192a899060eda52d6de8
Dazhy Khovalyg <hovalyg_d@mail.ru>
2025-02-24 22:31:32 +0300

    dev04

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -19,10 +19,60 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
-import "fmt"
+// findAnagrams ищет все группы анаграмм в списке слов
+func findAnagrams(words []string) map[string][]string {
+	anagramGroups := make(map[string][]string)
+	seenWords := make(map[string]bool)
 
+	for _, word := range words {
+		lowerWord := strings.ToLower(word)
+		sortedWord := sortString(lowerWord)
+
+		if !seenWords[lowerWord] {
+			anagramGroups[sortedWord] = append(anagramGroups[sortedWord], lowerWord)
+			seenWords[lowerWord] = true
+		}
+	}
+
+	return filterAnagramGroups(anagramGroups)
+}
+
+// sortString сортирует буквы в слове в алфавитном порядке
+func sortString(s string) string {
+	runes := []rune(s)
+	sort.Slice(runes, func(i, j int) bool {
+		return runes[i] < runes[j]
+	})
+	return string(runes)
+}
+
+// filterAnagramGroups удаляет группы, где только одно слово, и сортирует группы
+func filterAnagramGroups(groups map[string][]string) map[string][]string {
+	result := make(map[string][]string)
+	for _, group := range groups {
+		if len(group) > 1 {
+			sort.Strings(group)
+			result[group[0]] = group
+		}
+	}
+	return result
+}
+
+// printAnagrams выводит результат
+func printAnagrams(anagrams map[string][]string) {
+	for key, group := range anagrams {
+		fmt.Printf("%s: %v\n", key, group)
+	}
+}
 
 func main() {
-
+	words := []string{"пятак", "пятка", "пятка", "тяпка", "листок", "слиток", "столик", "слово"}
+	anagrams := findAnagrams(words)
+	printAnagrams(anagrams)
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"reflect"
+	"testing"
+)
 
+func TestFindAnagrams(t *testing.T) {
+	tests := []struct {
+		input    []string
+		expected map[string][]string
+	}{
+		{
+			input: []string{"пятак", "пятка", "тяпка", "листок", "слиток", "столик"},
+			expected: map[string][]string{
+				"пятак":  {"пятак", "пятка", "тяпка"},
+				"листок": {"листок", "слиток", "столик"},
+			},
+		},
+		{
+			input: []string{"пятак", "пятка", "пятка", "тяпка", "Пятак"},
+			expected: map[string][]string{
+				"пятак": {"пятак", "пятка", "тяпка"},
+			},
+		},
+		{
+			input:    []string{"слово"},
+			expected: map[string][]string{},
+		},
+		{
+			input:    []string{},
+			expected: map[string][]string{},
+		},
+	}
 
-func main() {
-
+	for _, test := range tests {
+		result := findAnagrams(test.input)
+		if !reflect.DeepEqual(result, test.expected) {
+			t.Errorf("Для входных данных %v\nожидалось %v, но получено %v", test.input, test.expected, result)
+		}
+	}
 }
